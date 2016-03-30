3e32490f9138485f6a91bb0af5d27d0d82757f8b
Aundra Miller and Torrence Stratton <tlstratton@gmail.com>
2016-03-30 08:58:55 -0700

    add initial project files, create index route, and add firebase db

---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('post');
+  },
+  actions: {
+    saveData() {
+      var params = {
+        title: "New Game: Blokus Trigon",
+        body: "Lorem ipsum dolor si amet",
+        date: "2016-03-31",
+        author: "Torrence Stratton",
+        image: "http://lorempixel.com/400/200"
+      };
+
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+      this.transitionTo('index');
+    }
+  }
+});
